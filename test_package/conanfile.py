2f2ed56fc5f3178150fda3553086ef79e620321c
Khalil Estell <kammce0@gmail.com>
2025-12-08 10:40:38 -0800

    :pushpin: Cleanup some of the code & dependencies (#31)

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -16,29 +16,23 @@
 
 from conan import ConanFile
 from conan.tools.build import cross_building
-from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
+from conan.tools.cmake import CMake, cmake_layout
 from pathlib import Path
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "VirtualRunEnv"
+    generators = "VirtualRunEnv", "CMakeDeps", "CMakeToolchain"
 
     def build_requirements(self):
-        self.tool_requires("cmake-modules-toolchain/1.0.2")
+        self.tool_requires("cmake/[^4.0.0]")
+        self.tool_requires("ninja/[^1.3.0]")
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
     def layout(self):
         cmake_layout(self)
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
-
-        deps = CMakeDeps(self)
-        deps.generate()
 
     def build(self):
         cmake = CMake(self)
